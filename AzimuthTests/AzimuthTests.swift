bc26014111de604a9d37dbb2c65542e2a8b30999
JDeWeese <jdeweese574@icloud.com>
2024-10-26 08:08:10 -0500

    Initial Commit

---
+++ AzimuthTests/AzimuthTests.swift
@@ -0,0 +1,16 @@
+//
+//  AzimuthTests.swift
+//  AzimuthTests
+//
+//  Created by Joseph DeWeese on 10/26/24.
+//
+
+import Testing
+
+struct AzimuthTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
